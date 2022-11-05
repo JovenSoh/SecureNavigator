75595dc681d3010afd2c6c499f7b63d7aea9f63d
tkaixiang <tkaixiang@gmail.com>
2022-11-05 22:30:34 +0800

    simple xss check

--- extension.js
+++ extension.js
@@ -31,67 +31,81 @@ class Emojizer {
 
 		// Call ML model here
 		if (context.diagnostics.length > 0) {
-			if (context.diagnostics[0].code) {
-				const hashedInput = createHash('sha256').update(context.diagnostics[0].code).digest('hex')
 
-				console.log("Received: " + context.diagnostics[0].code)
-				let varType = "let "
-				let generatedFix = ""
+			const code = context.diagnostics[0].code
+			if (code) {
 
-				for (let i = 0; i < varTypes.length; i++) {
-					if (varTypes[i] === context.diagnostics[0].code.slice(0, varTypes[i].length)) {
-						varType = varTypes[i] + " "
-						break
-					}
-				}
+				let action = null
+				if (code === "xss-error") {
+					console.log("xss-error code-action")
 
-				if (hashedInput in savedResponses) {
-					generatedFix = savedResponses[hashedInput]
+					action = new vscode.CodeAction('Learn more about how to fix XSS errors', vscode.CodeActionKind.QuickFix);
+					action.command = { command: 'vulnCheck.openXSS', title: 'Learn more about how to fix XSS errors', tooltip: 'This will open the DOM-Purify page.' };
+					action.isPreferred = true
 				}
 				else {
-					const spawn = require("child_process").spawn;
-					const pythonProcess = spawn(pythonCommand, ["prediction.py", context.diagnostics[0].code], { cwd: __dirname });
+					const hashedInput = createHash('sha256').update(context.diagnostics[0].code).digest('hex')
 
+					console.log("Received: " + context.diagnostics[0].code)
+					let varType = "let "
+					let generatedFix = ""
 
-
-					pythonProcess.on("error", (err) => {
-						console.log("Error occured in python script")
-						console.error(err)
-					})
-
-
-					generatedFix = await new Promise((resolve, reject) => {
-						let finalData = ""
-						pythonProcess.stdout.on('data', (data) => {
-							finalData += data.toString()
-							//console.log("data: " + data)
-						});
-						pythonProcess.stderr.on('data', (data) => {
-							//console.log("error: " + data) 
-						});
-						pythonProcess.stdout.on("end", (data) => {
-							//console.log("end data: " + finalData)
-							resolve(finalData)
-						})
-					});
-
-					// Clear cache once size gets too large
-					const size = Object.keys(savedResponses).length;
-					if (size > 50) {
-						savedResponses = {}
+					for (let i = 0; i < varTypes.length; i++) {
+						if (varTypes[i] === context.diagnostics[0].code.slice(0, varTypes[i].length)) {
+							varType = varTypes[i] + " "
+							break
+						}
 					}
 
-					console.log("final output: " + generatedFix)
-					savedResponses[hashedInput] = generatedFix
+					if (hashedInput in savedResponses) {
+						generatedFix = savedResponses[hashedInput]
+					}
+					else {
+						const spawn = require("child_process").spawn;
+						const pythonProcess = spawn(pythonCommand, ["prediction.py", context.diagnostics[0].code], { cwd: __dirname });
+
+
+
+						pythonProcess.on("error", (err) => {
+							console.log("Error occured in python script")
+							console.error(err)
+						})
+
+
+						generatedFix = await new Promise((resolve, reject) => {
+							let finalData = ""
+							pythonProcess.stdout.on('data', (data) => {
+								finalData += data.toString()
+								//console.log("data: " + data)
+							});
+							pythonProcess.stderr.on('data', (data) => {
+								//console.log("error: " + data) 
+							});
+							pythonProcess.stdout.on("end", (data) => {
+								//console.log("end data: " + finalData)
+								resolve(finalData)
+							})
+						});
+
+						// Clear cache once size gets too large
+						const size = Object.keys(savedResponses).length;
+						if (size > 50) {
+							savedResponses = {}
+						}
+
+						console.log("final output: " + generatedFix)
+						savedResponses[hashedInput] = generatedFix
+					}
+
+					const action = new vscode.CodeAction(`Fix SQLi!`, vscode.CodeActionKind.QuickFix);
+					action.edit = new vscode.WorkspaceEdit();
+
+					action.edit.replace(document.uri, range, varType + generatedFix);
+					action.isPreferred = true
 				}
 
-				const fix = new vscode.CodeAction(`Fix SQLi!`, vscode.CodeActionKind.QuickFix);
-				fix.edit = new vscode.WorkspaceEdit();
+				return [action]
 
-				fix.edit.replace(document.uri, range, varType + generatedFix);
-				fix.isPreferred = true
-
-				return [fix];
 			}
 
 		}
@@ -116,6 +130,8 @@ const raiseSQLiError = async (SQLWarningDecorationType, range, activeEditor, col
 		]
 	}]);
 }
+
+
 
 // Called when the activation event occurs (which is calling the command in this case)
 function activate(context) {
@@ -145,15 +161,16 @@ function activate(context) {
 		const collection = vscode.languages.createDiagnosticCollection('sqli-detector');
 		context.subscriptions.push(collection)
 
-		const debouncedFunction = debounce(raiseSQLiError, 500)
-
-		vscode.workspace.onDidChangeTextDocument(() => {
-
+		// Function for handling typing changes, it is passed through a debouncer
+		const onChangeHandler = () => {
 			const activeEditor = vscode.window.activeTextEditor
 			if (activeEditor) {
 				const currentLine = activeEditor.selection.active.line
 				const contents = activeEditor.document.lineAt(currentLine)._text
 
+				// Clear first whenever there are changes
+				collection.clear()
+				activeEditor.setDecorations(SQLWarningDecorationType, [{ range: null }])
 
 				// Detected possible SQLi
 				if (SQLiRegex.test(contents)) {
@@ -170,18 +187,48 @@ function activate(context) {
 
 					if (noError) debouncedFunction(SQLWarningDecorationType, range, activeEditor, collection, contents)
 				}
-				else {
-					// Clear if current line has no SQL error/no longer has it
-					collection.clear()
-					activeEditor.setDecorations(SQLWarningDecorationType, [{ range: null }])
+				else if (contents.indexOf("innerHTML") !== -1) {
+					const range = new vscode.Range(new vscode.Position(currentLine, 0), new vscode.Position(currentLine, contents.length))
+					activeEditor.setDecorations(SQLWarningDecorationType, [{ range: range }])
+
+					collection.set(activeEditor.document.uri, [{
+						code: 'xss-error',
+						message: 'Possible XSS vulnerability detected! Remember to sanitize the input if it is user controlled using a library such as DOM-purify!',
+						range: range,
+						severity: vscode.DiagnosticSeverity.Warning,
+						source: '',
+						relatedInformation: [
+							new vscode.DiagnosticRelatedInformation(new vscode.Location(activeEditor.document.uri, range), ' is a possible XSS vulnerability')
+						]
+					}]);
 				}
 			}
+		}
+
+		// Command function for opening the DomPurify page
+		const openDOMPage = () => {
+			vscode.env.openExternal(vscode.Uri.parse('https://github.com/cure53/DOMPurify'))
+		}
+
+		const debouncedFunction = debounce(raiseSQLiError, 500)
+		const debouncedTypingFunction = debounce(onChangeHandler, 50)
+
+		vscode.workspace.onDidChangeTextDocument(() => {
+			debouncedTypingFunction()
 		})
 
 		context.subscriptions.push(
 			vscode.languages.registerCodeActionsProvider('javascript', new Emojizer(), {
 				providedCodeActionKinds: Emojizer.providedCodeActionKinds
 			})
+		);
+		context.subscriptions.push(
+			vscode.languages.registerCodeActionsProvider('html', new Emojizer(), {
+				providedCodeActionKinds: Emojizer.providedCodeActionKinds
+			})
+		);
+		context.subscriptions.push(
+			vscode.commands.registerCommand('vulnCheck.openXSS', openDOMPage)
 		);
 	}
 	catch (e) {
